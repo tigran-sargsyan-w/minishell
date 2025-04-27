8ee3c6453e3188d7b8c0f102b27866a1733ed118
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-04-27 14:29:25 +0200

    feat: implement test functions for gnl, ft_printf, and libft; add test.txt

--- include/minishell.h
+++ include/minishell.h
@@ -6,13 +6,18 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 14:09:40 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/27 14:10:06 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/04/27 14:24:38 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include "libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,14 +6,70 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 14:08:36 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/27 14:09:23 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/04/27 14:27:33 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(void)
+// function to test gnl from test.txt
+void	test_gnl(void)
 {
-    printf("Welcome to the shell!\n");
-    return (0);
+	int		fd;
+	char	*line;
+
+	printf("TESTING GNL\n");
+	fd = open("test.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error opening file");
+		return ;
+	}
+	line = get_next_line(fd);
+	printf("First line: %s", line);
+	line = get_next_line(fd);
+	if (line == NULL)
+	{
+		printf("No line found\n");
+		free(line);
+		close(fd);
+		return ;
+	}
+	printf("Second line: %s", line);
+	free(line);
+	close(fd);
+}
+
+// function to test ft_printf
+void	test_printf(void)
+{
+	int		num;
+	char	*str;
+
+	num = 42;
+	str = "Hello, World!";
+	printf("\nTESTING FT_PRINTF\n");
+	ft_printf("Integer: %d\n", num);
+	ft_printf("String: %s\n", str);
+}
+
+// function to test libft functions
+void	test_libft(void)
+{
+	char	*str;
+
+	str = "Hello, World!";
+	printf("\nTESTING LIBFT\n");
+	printf("Length of string: %zu\n", ft_strlen(str));
+	printf("Is 'H' alpha: %d\n", ft_isalpha('H'));
+	printf("Is 'H' digit: %d\n", ft_isdigit('H'));
+}
+
+int	main(void)
+{
+	test_gnl();
+	test_printf();
+	test_libft();
+	printf("\nWelcome to the shell!\n");
+	return (0);
 }
