2448478f12e80fb7ceb266123aea56829d9c2645
dsemenov <dsemenov@student.42lyon.fr>
2025-04-28 18:57:43 +0200

    Fix(project): remove tests for libft / gnl / printf

--- include/tests.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTS_H
-# define TESTS_H
-
-void	test_gnl(void);
-void	test_printf(void);
-void	test_libft(void);
-
-#endif
--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 14:08:36 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/28 18:17:05 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/04/28 18:55:32 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,11 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "libft.h"
-//#include "tests.h"
 
 int	main(int argc, char **argv, char **envp)
 {
   char  *input_str;
 
-	/*test_gnl();*/
-	/*test_printf();*/
-	/*test_libft();*/
   while (1)
   {
     input_str = readline("minishell > ");
