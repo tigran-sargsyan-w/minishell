bda8b54813dc74e12ea42bd9423df4bff4e6779e
dsemenov <dsemenov@student.42lyon.fr>
2025-04-29 18:07:54 +0200

    feat(builtins): add printf to see env variables. Create echo.c in folder builtins

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/27 14:08:36 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/29 18:07:02 by dsemenov         ###   ########.fr       */
+/*   Created: 2025/04/29 18:02:24 by dsemenov          #+#    #+#             */
+/*   Updated: 2025/04/29 18:05:10 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-int	main(int argc, char **argv, char **envp)
-{
-	char	*input_str;
-
-	(void)argc;
-	(void)argv;
-	while (envp[0] != NULL)
-	{
-		printf("%s\n", envp[0]);
-		envp++;
-	}
-	(void)envp;
-	input_str = NULL;
-	readline_loop(&input_str);
-	return (0);
-}
