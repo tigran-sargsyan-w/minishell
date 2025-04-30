58b86db4c1fe82906fad374f439ba55ef6ecef99
dsemenov <dsemenov@student.42lyon.fr>
2025-04-30 15:07:34 +0200

    fix: remove test in main. Add echo.c file

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/27 14:08:36 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/04/30 13:09:46 by dsemenov         ###   ########.fr       */
+/*   Created: 2025/04/29 18:02:24 by dsemenov          #+#    #+#             */
+/*   Updated: 2025/04/29 18:05:10 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include "minishell.h"
-
-int	main(int argc, char **argv, char **envp)
-{
-	char	*input_str;
-
-	(void)argc;
-	(void)argv;
-	while (envp[0] != NULL)
-	{
-		printf("%s\n", envp[0]);
-		envp++;
-	}
-	(void)envp;
-	input_str = NULL;
-	readline_loop(&input_str);
-	return (0);
-}
