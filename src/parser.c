73f955c2abc1e8582421523ce53465e01dd17f3d
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-05-01 16:45:12 +0200

    fix: reorder includes and clean up whitespace in parser.c

--- src/parser.c
+++ src/parser.c
@@ -6,23 +6,25 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:58:43 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/05/01 16:40:10 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/01 16:44:38 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parser.h"
 #include "libft.h"
+#include "parser.h"
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int	expand_args(t_cmd *cmd)
 {
-	int		old_cap = cmd->arg_cap;
-	int		new_cap = old_cap * 2;
+	int		old_cap;
+	int		new_cap;
 	char	**new_args;
 	int		i;
 
+	old_cap = cmd->arg_cap;
+	new_cap = old_cap * 2;
 	new_args = malloc(sizeof(char *) * new_cap);
 	if (!new_args)
 		return (1);
@@ -34,7 +36,6 @@ int	expand_args(t_cmd *cmd)
 	}
 	while (i < new_cap)
 		new_args[i++] = NULL;
-
 	free(cmd->args);
 	cmd->args = new_args;
 	cmd->arg_cap = new_cap;
@@ -43,8 +44,9 @@ int	expand_args(t_cmd *cmd)
 
 int	append_arg(t_cmd *cmd, char *value)
 {
-	int	i = 0;
+	int	i;
 
+	i = 0;
 	while (cmd->args[i])
 		i++;
 	if (i + 1 >= cmd->arg_cap)
@@ -59,7 +61,6 @@ int	append_arg(t_cmd *cmd, char *value)
 	return (0);
 }
 
-
 t_cmd	*parse_tokens(t_token *tokens)
 {
 	t_cmd	*cmd;
@@ -70,7 +71,6 @@ t_cmd	*parse_tokens(t_token *tokens)
 	cmd = malloc(sizeof(t_cmd));
 	if (!cmd)
 		return (NULL);
-
 	cmd->arg_cap = INITIAL_ARG_CAP;
 	cmd->args = malloc(sizeof(char *) * cmd->arg_cap);
 	if (!cmd->args)
@@ -78,15 +78,12 @@ t_cmd	*parse_tokens(t_token *tokens)
 	i = 0;
 	while (i < cmd->arg_cap)
 		cmd->args[i++] = NULL;
-
 	cmd->infile = NULL;
 	cmd->outfile = NULL;
 	cmd->append = 0;
 	cmd->heredoc = 0;
 	cmd->next = NULL;
-
 	current_cmd = cmd;
-
 	while (tokens)
 	{
 		if (tokens->type == WORD)
@@ -119,12 +116,13 @@ t_cmd	*parse_tokens(t_token *tokens)
 		}
 		else if (tokens->type == PIPE)
 		{
-            // TODO: logic for creating a new command
-            // TODO: handle pipe
+			// TODO: logic for creating a new command
+			// TODO: handle pipe
 		}
 		else
 		{
-			printf("minishell: syntax error near unexpected token `%s'\n", tokens->value);
+			printf("minishell: syntax error near unexpected token `%s'\n",
+				tokens->value);
 			// TODO: free_cmd_list(cmd);
 			return (NULL);
 		}
