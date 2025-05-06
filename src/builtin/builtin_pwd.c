17e71631706af463468e11a2117b1bc3fc487bf4
dsemenov <dsemenov@student.42lyon.fr>
2025-05-06 21:02:48 +0200

    fix: handle error in builtin_pwd when getcwd fails

--- src/builtin/builtin_pwd.c
+++ src/builtin/builtin_pwd.c
@@ -6,24 +6,28 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:52:23 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/05/06 19:27:13 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/05/06 20:57:27 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
-#include "unistd.h"
 #include <stdio.h>
-#include <limits.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int	builtin_pwd(t_cmd *cmd, t_env_list *env)
 {
-	(void)cmd;
 	(void)env;
+	(void)cmd;
 	char	*cwd;
 
 	cwd = getcwd(NULL, 0);
+	if (cwd == NULL)
+	{
+		perror("pwd");
+		return (EXIT_FAILURE);
+	}
 	printf("%s\n", cwd);
 	free(cwd);
-	return (0);
+	return (EXIT_SUCCESS);
 }
