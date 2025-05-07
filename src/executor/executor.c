b7b1757ebc0e43a4b753c4dbc352aedf4c032891
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-05-07 12:53:09 +0200

    feat: implement input and output redirection handling in command execution

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 12:38:33 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/05/06 14:42:50 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/07 12:51:46 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,40 @@
 #include <sys/types.h> // pid_t
 #include <sys/wait.h>  // waitpid
 #include <unistd.h>    // fork, execve
+#include <fcntl.h>     // open, O_RDONLY, O_WRONLY, O_CREAT, O_TRUNC, O_APPEND
+
+void	handle_redirections(t_cmd *cmd)
+{
+	int	fd;
+
+	// Input redirection: <
+	if (cmd->infile)
+	{
+		fd = open(cmd->infile, O_RDONLY);
+		if (fd < 0)
+		{
+			perror("minishell: open infile");
+			exit(1);
+		}
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+	}
+	// Output redirection: > or >>
+	if (cmd->outfile)
+	{
+		if (cmd->append)
+			fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		else
+			fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd < 0)
+		{
+			perror("minishell: open outfile");
+			exit(1);
+		}
+		dup2(fd, STDOUT_FILENO);
+		close(fd);
+	}
+}
 
 void	execute_cmd(t_cmd *cmd, char **envp)
 {
@@ -35,6 +69,7 @@ void	execute_cmd(t_cmd *cmd, char **envp)
 	}
 	if (pid == 0)
 	{
+		handle_redirections(cmd);
 		full_cmd = find_command(cmd->args[0], envp);
 		if (!full_cmd)
 		{
