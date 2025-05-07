28b3e02a47012f909337e0834d4e9e4827fb9b7a
dsemenov <dsemenov@student.42lyon.fr>
2025-05-07 18:38:45 +0200

    feat: enhance builtin_export to handle environment variable updates and additions

--- src/builtin/builtin_export.c
+++ src/builtin/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:52:15 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/05/07 17:46:38 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/05/07 18:38:28 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,62 @@
 #include "parser.h"
 #include <stddef.h>
 #include <stdio.h>
+#include "libft.h"
+#include <stdlib.h>
 
-int	builtin_export(t_cmd *cmd, t_env_list **env)
+static int is_in_envlist(t_env_list **env, char *name, char *value)
 {
-	(void)cmd;
 	t_env_list	*tmp;
-	
-	if (env == NULL || *env == NULL)
-		return (1);
-	if (cmd->args == NULL)
-	{
-		tmp = *env;
-		while (tmp)
-		{
-			printf("declare -x %s=\"%s\"\n", tmp->key, tmp->value);
-			tmp = tmp->next;
-		}
-		return (0);
-	}
+
 	tmp = *env;
 	while (tmp)
 	{
-		lst_create_node(tmp->key, tmp->value);
+		if (ft_strcmp(tmp->key, name) == 0)
+		{
+			tmp->value = ft_strdup(value);
+			if (tmp->value == NULL)
+			{
+				perror("malloc");
+				return (-1);
+			}
+			return (1);
+		}
+		tmp = tmp->next;
 	}
+	return (0);
+}
+
+int	builtin_export(t_cmd *cmd, t_env_list **env)
+{
+	t_env_list	*new_node;
+	char		*equality_sign;
+	char		*key;
+	char		*value;
+	int			key_len;
+	
+	if (env == NULL || *env == NULL)
+		return (1);
+	equality_sign = ft_strchr(cmd->args[1], '=');
+	if (equality_sign == NULL)
+	{
+		ft_putendl_fd("export: not enough arguments", 2);
+		return (1);
+	}
+	key_len = equality_sign - (cmd->args[1]);
+	key = ft_substr(cmd->args[1], 0, key_len);
+	value = ft_strdup(++equality_sign);
+	if (key == NULL || value == NULL)
+	{
+		perror("malloc");
+		return (1);
+	}
+	new_node = lst_create_node(key, value);
+	if (new_node == NULL)
+	{
+		perror("malloc");
+		return (1);
+	}
+	if (is_in_envlist(env, key, value) == 0)
+		lst_add_end(env, new_node);
 	return (0);
 }
