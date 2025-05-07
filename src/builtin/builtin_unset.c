0cd22a8f871c4a445e351acb84f89148cf37cad8
dsemenov <dsemenov@student.42lyon.fr>
2025-05-07 19:32:13 +0200

    feat: implement error handling and environment variable removal in builtin_unset

--- src/builtin/builtin_unset.c
+++ src/builtin/builtin_unset.c
@@ -6,16 +6,45 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:52:38 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/05/07 14:29:01 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/05/07 19:31:15 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include "parser.h"
+#include "libft.h"
+#include <stdlib.h>
 
 int	builtin_unset(t_cmd *cmd, t_env_list **env)
 {
-	(void)cmd;
-	(void)env;
+	t_env_list	*cur;
+	t_env_list	*prev;
+
+	if (cmd->args[1] == NULL)
+	{
+		ft_putendl_fd("unset: not enough arguments", 2);
+		return (1);
+	}
+	if (env == NULL || *env == NULL)
+		return (0);
+	cur = *env;
+	prev = NULL;
+	while (cur && ft_strcmp(cur->key, cmd->args[1]) != 0)
+	{
+
+		prev = cur;
+		cur = cur->next;
+	}
+	if (cur)
+	{
+		if (prev)
+			prev->next = cur->next;
+		else
+			*env = cur->next;
+		free(cur->key);
+		free(cur->value);
+		free(cur);
+		return (0);
+	}
 	return (0);
 }
