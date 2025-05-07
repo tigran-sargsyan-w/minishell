44b24b56efdc12b58dadc208827f198e3559cd17
dsemenov <dsemenov@student.42lyon.fr>
2025-05-07 19:05:25 +0200

    feat: improve builtin_export to handle missing arguments and free memory correctly

--- src/builtin/builtin_export.c
+++ src/builtin/builtin_export.c
@@ -6,18 +6,18 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:52:15 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/05/07 18:38:28 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/05/07 19:03:02 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
+#include "libft.h"
 #include "parser.h"
 #include <stddef.h>
 #include <stdio.h>
-#include "libft.h"
 #include <stdlib.h>
 
-static int is_in_envlist(t_env_list **env, char *name, char *value)
+static int	is_in_envlist(t_env_list **env, char *name, char *value)
 {
 	t_env_list	*tmp;
 
@@ -26,6 +26,7 @@ static int is_in_envlist(t_env_list **env, char *name, char *value)
 	{
 		if (ft_strcmp(tmp->key, name) == 0)
 		{
+			free(tmp->value);
 			tmp->value = ft_strdup(value);
 			if (tmp->value == NULL)
 			{
@@ -46,9 +47,13 @@ int	builtin_export(t_cmd *cmd, t_env_list **env)
 	char		*key;
 	char		*value;
 	int			key_len;
-	
-	if (env == NULL || *env == NULL)
+	int			ret;
+
+	if (cmd->args[1] == NULL)
+	{
+		ft_putendl_fd("export: not enough arguments", 2);
 		return (1);
+	}
 	equality_sign = ft_strchr(cmd->args[1], '=');
 	if (equality_sign == NULL)
 	{
@@ -61,15 +66,31 @@ int	builtin_export(t_cmd *cmd, t_env_list **env)
 	if (key == NULL || value == NULL)
 	{
 		perror("malloc");
+		free(key);
+		free(value);
 		return (1);
+	}
+	ret = is_in_envlist(env, key, value);
+	if (ret < 0)
+	{
+		free(key);
+		free(value);
+		return (1);
+	}
+	else if (ret == 1)
+	{
+		free(key);
+		free(value);
+		return (0);
 	}
 	new_node = lst_create_node(key, value);
 	if (new_node == NULL)
 	{
 		perror("malloc");
+		free(key);
+		free(value);
 		return (1);
 	}
-	if (is_in_envlist(env, key, value) == 0)
-		lst_add_end(env, new_node);
+	lst_add_end(env, new_node);
 	return (0);
 }
