4a287552dc8220f41027ba2094346b643d3b11df
dsemenov <dsemenov@student.42lyon.fr>
2025-05-14 19:51:30 +0200

    fix: add error handling for invalid arguments in builtin_env

--- src/builtin/builtin_env.c
+++ src/builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:51:59 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/05/07 17:49:53 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/05/14 19:51:00 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ int	builtin_env(t_cmd *cmd, t_env_list **env)
 	
 	if (env == NULL || *env == NULL)
 		return (1);
+	if (cmd->args[1])
+	{
+		//TODO: Print correct error message - env: ‘example_arg’: No such file or directory
+		return (127);
+	}
 	tmp = *env;
 	while (tmp)
 	{
