0637d9648cee4d244ccc0567ffa186dd72a72eab
dsemenov <dsemenov@student.42lyon.fr>
2025-05-15 17:29:23 +0200

    feat: update env_list_to_tab function to correctly populate environment array

--- include/env.h
+++ include/env.h
@@ -6,7 +6,7 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 16:03:21 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/05/15 16:52:48 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/05/15 16:57:37 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ void					lst_clear(t_env_list **list);
 t_env_list				*lst_create_node(char *key, char *value);
 void					lst_add_end(t_env_list **list, t_env_list *new_node);
 size_t					lst_size(t_env_list **list);
+char					**env_list_to_tab(t_env_list **env_list);
 
 #endif
--- src/env/env_list_to_tab.c
+++ src/env/env_list_to_tab.c
@@ -6,14 +6,14 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 16:00:25 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/05/15 16:53:39 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/05/15 17:23:10 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
 #include "libft.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static char	*ft_strjoin3(char const *s1, char const *s2)
 {
@@ -26,32 +26,34 @@ static char	*ft_strjoin3(char const *s1, char const *s2)
 		return (NULL);
 	res[0] = '\0';
 	ft_strlcat(res, s1, total_len + 1);
-    ft_strlcat(res, "=", 2);
+	ft_strlcat(res, "=", total_len + 1);
 	ft_strlcat(res, s2, total_len + 1);
 	return (res);
 }
 
-char    **env_list_to_tab(t_env_list **env_list)
+char	**env_list_to_tab(t_env_list **env_list)
 {
-    char        **env_tab;
-    size_t      size_of_list;
-    t_env_list  *tmp;
+	char **env_tab;
+	size_t size_of_list;
+	t_env_list *tmp;
+	size_t i;
 
-    tmp = *env_list;
-    if ((size_of_list = lst_size(env_list)) == 0)
-        return (NULL);
-    env_tab = malloc(sizeof(char *) * size_of_list + 1);
-    if (env_tab == NULL)
-    {
-        perror("minishell:");
-        return (NULL);
-    }
-    env_tab[size_of_list + 1] = NULL; 
-    while (tmp)
-    {
-        *env_tab = ft_strjoin3(tmp->key, tmp->value); 
-        tmp = tmp->next;
-        (*env_tab)++;
-    }
-    return (env_tab);
+	tmp = *env_list;
+	if ((size_of_list = lst_size(env_list)) == 0)
+		return (NULL);
+	env_tab = malloc(sizeof(char *) * size_of_list + 1);
+	if (env_tab == NULL)
+	{
+		perror("minishell:");
+		return (NULL);
+	}
+	i = 0;
+	env_tab[size_of_list + 1] = NULL;
+	while (tmp && i < size_of_list)
+	{
+		env_tab[i] = ft_strjoin3(tmp->key, tmp->value);
+		tmp = tmp->next;
+		i++;
+	}
+	return (env_tab);
 }
