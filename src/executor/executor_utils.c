001c498fdc27a8cec08caebc095366e7f469f30f
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-05-23 11:46:16 +0200

    chore: stub for the executor so that the parser can be compiled and tested

--- src/executor/executor_utils.c
+++ src/executor/executor_utils.c
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:19:46 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/05/22 17:03:48 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/23 10:51:53 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,61 +21,64 @@
 
 void	handle_heredoc_file(t_cmd *cmd)
 {
-	int		fd;
-	char	*line;
+	(void)cmd;
+	// int		fd;
+	// char	*line;
 
-	fd = open(HEREDOC_TMPFILE, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (fd < 0)
-		error_exit("heredoc");
-	while (1)
-	{
-		line = readline("heredoc> ");
-		if (!line || ft_strcmp(line, cmd->infile) == 0)
-		{
-			free(line);
-			break ;
-		}
-		write(fd, line, ft_strlen(line));
-		write(fd, "\n", 1);
-		free(line);
-	}
-	close(fd);
-	free(cmd->infile);
-	cmd->infile = ft_strdup(HEREDOC_TMPFILE);
-	cmd->heredoc = 0;
+	// fd = open(HEREDOC_TMPFILE, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	// if (fd < 0)
+	// 	error_exit("heredoc");
+	// while (1)
+	// {
+	// 	line = readline("heredoc> ");
+	// 	if (!line || ft_strcmp(line, cmd->infile) == 0)
+	// 	{
+	// 		free(line);
+	// 		break ;
+	// 	}
+	// 	write(fd, line, ft_strlen(line));
+	// 	write(fd, "\n", 1);
+	// 	free(line);
+	// }
+	// close(fd);
+	// free(cmd->infile);
+	// cmd->infile = ft_strdup(HEREDOC_TMPFILE);
+	// cmd->heredoc = 0;
 }
 
 void	handle_input_redirection(t_cmd *cmd)
 {
-	int	fd;
+	(void)cmd;
+	// int	fd;
 
-	if (cmd->heredoc && cmd->infile)
-		handle_heredoc_file(cmd);
-	if (cmd->infile)
-	{
-		fd = open(cmd->infile, O_RDONLY);
-		if (fd < 0)
-			error_exit("open infile");
-		dup2(fd, STDIN_FILENO);
-		close(fd);
-	}
+	// if (cmd->heredoc && cmd->infile)
+	// 	handle_heredoc_file(cmd);
+	// if (cmd->infile)
+	// {
+	// 	fd = open(cmd->infile, O_RDONLY);
+	// 	if (fd < 0)
+	// 		error_exit("open infile");
+	// 	dup2(fd, STDIN_FILENO);
+	// 	close(fd);
+	// }
 }
 
 void	handle_output_redirection(t_cmd *cmd)
 {
-	int	fd;
+	(void)cmd;
+	// int	fd;
 
-	if (cmd->outfile)
-	{
-		if (cmd->append)
-			fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		else
-			fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd < 0)
-			error_exit("open outfile");
-		dup2(fd, STDOUT_FILENO);
-		close(fd);
-	}
+	// if (cmd->outfile)
+	// {
+	// 	if (cmd->append)
+	// 		fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	// 	else
+	// 		fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	// 	if (fd < 0)
+	// 		error_exit("open outfile");
+	// 	dup2(fd, STDOUT_FILENO);
+	// 	close(fd);
+	// }
 }
 
 void	execute_child(t_cmd *cmd, t_shell *sh)
