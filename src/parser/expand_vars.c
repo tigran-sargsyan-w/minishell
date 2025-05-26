eec39e89084a03cc3186efc8b603d31f062ae23e
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-05-26 20:23:02 +0200

    refactor: improve code formatting and organization in expand_vars function

--- src/parser/expand_vars.c
+++ src/parser/expand_vars.c
@@ -6,119 +6,120 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 13:42:50 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/05/26 20:04:18 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/26 20:08:09 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "env.h"
+#include "executor.h"
 #include "libft.h"
 #include "minishell.h"
-#include "env.h"
 #include <stdlib.h>
-#include "executor.h"
 
-static int  handle_exit_status(const char *input, size_t *i, char **res, t_shell *sh);
-static int  handle_env_var(const char *input, size_t *i, char **res, t_shell *sh);
-static void append_char_to_result(const char *input, size_t *pos, char **result);
-static char *get_env_value(const char *var_name, t_env_list *env_list);
+static int	handle_exit_status(const char *input, size_t *i, char **res,
+				t_shell *sh);
+static int	handle_env_var(const char *input, size_t *i, char **res,
+				t_shell *sh);
+static void	append_char_to_result(const char *input, size_t *pos,
+				char **result);
+static char	*get_env_value(const char *var_name, t_env_list *env_list);
 
-static char *get_env_value(const char *var_name, t_env_list *env_list)
+static char	*get_env_value(const char *var_name, t_env_list *env_list)
 {
-    t_env_list *tmp = env_list;
-    size_t      len;
+	t_env_list	*tmp;
+	size_t		len;
 
-    while (tmp)
-    {
-        len = ft_strlen(tmp->key); 
-        if (ft_strncmp(tmp->key, var_name, len + 1) == 0)
-            return tmp->value;
-        tmp = tmp->next;
-    }
-    return ("");
+	tmp = env_list;
+	while (tmp)
+	{
+		len = ft_strlen(tmp->key);
+		if (ft_strncmp(tmp->key, var_name, len + 1) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return ("");
 }
 
 // 1) $?
-static int handle_exit_status(const char *input, size_t *pos, char **result, t_shell *sh)
+static int	handle_exit_status(const char *input, size_t *pos, char **result,
+		t_shell *sh)
 {
-    if (input[*pos + 1] != '?')
-        return (FAILURE);
+	char	*status_str;
+	char	*tmp;
 
-    char *status_str = ft_itoa(sh->last_status);
-    if (!status_str)
-        return (FAILURE);
-
-    char *tmp = *result;
-    *result = ft_strjoin(*result, status_str);
-    free(tmp);
-    free(status_str);
-
-    *pos += 2;
-    return (SUCCESS);
+	if (input[*pos + 1] != '?')
+		return (FAILURE);
+	status_str = ft_itoa(sh->last_status);
+	if (!status_str)
+		return (FAILURE);
+	tmp = *result;
+	*result = ft_strjoin(*result, status_str);
+	free(tmp);
+	free(status_str);
+	*pos += 2;
+	return (SUCCESS);
 }
 
 // 2) $NAME
-static int handle_env_var(const char *input, size_t *pos, char **result, t_shell *sh)
+static int	handle_env_var(const char *input, size_t *pos, char **result,
+		t_shell *sh)
 {
-    size_t j = *pos + 1;
-    if (!(ft_isalnum((unsigned char)input[j]) || input[j] == '_'))
-        return (FAILURE);
+	size_t	j;
+	char	*var_name;
+	char	*val;
+	char	*tmp;
 
-    while (input[j] && (ft_isalnum((unsigned char)input[j]) || input[j] == '_'))
-        j++;
-
-    char *var_name = ft_substr(input, *pos + 1, j - (*pos + 1));
-    if (!var_name)
-        return (FAILURE);
-
-    char *val = get_env_value(var_name, sh->env_list);
-    free(var_name);
-
-    char *tmp = *result;
-    *result = ft_strjoin(*result, val);
-    free(tmp);
-
-    *pos = j;
-    return (SUCCESS);
+	j = *pos + 1;
+	if (!(ft_isalnum((unsigned char)input[j]) || input[j] == '_'))
+		return (FAILURE);
+	while (input[j] && (ft_isalnum((unsigned char)input[j]) || input[j] == '_'))
+		j++;
+	var_name = ft_substr(input, *pos + 1, j - (*pos + 1));
+	if (!var_name)
+		return (FAILURE);
+	val = get_env_value(var_name, sh->env_list);
+	free(var_name);
+	tmp = *result;
+	*result = ft_strjoin(*result, val);
+	free(tmp);
+	*pos = j;
+	return (SUCCESS);
 }
 
-static void append_char_to_result(const char *input, size_t *pos, char **result)
+static void	append_char_to_result(const char *input, size_t *pos, char **result)
 {
-    char    buf[2];
-    char   *old;
+	char	buf[2];
+	char	*old;
 
-    buf[0] = input[*pos];
-    buf[1] = '\0';
+	buf[0] = input[*pos];
+	buf[1] = '\0';
 	old = *result;
-    *result = ft_strjoin(old, buf);
-    free(old);
-    (*pos)++;
+	*result = ft_strjoin(old, buf);
+	free(old);
+	(*pos)++;
 }
 
-char *expand_vars(const char *input, t_shell *sh)
+char	*expand_vars(const char *input, t_shell *sh)
 {
-    char   *result;
-    size_t  pos;
+	char	*result;
+	size_t	pos;
 
-    result = ft_strdup("");
-    if (!result)
-        return (NULL);
-
-    pos = 0;
-    while (input[pos])
-    {
-        if (input[pos] == '$')
-        {
-            if (handle_exit_status(input, &pos, &result, sh) == SUCCESS)
-                continue;
-            if (handle_env_var(input, &pos, &result, sh) == SUCCESS)
-                continue;
-			// ни $? ни $NAME — вставляем просто '$'
-            append_char_to_result(input, &pos, &result);
-        }
-        else
-        {
-			// любой другой символ
+	result = ft_strdup("");
+	if (!result)
+		return (NULL);
+	pos = 0;
+	while (input[pos])
+	{
+		if (input[pos] == '$')
+		{
+			if (handle_exit_status(input, &pos, &result, sh) == SUCCESS)
+				continue ;
+			if (handle_env_var(input, &pos, &result, sh) == SUCCESS)
+				continue ;
 			append_char_to_result(input, &pos, &result);
-        }
-    }
-    return (result);
+		}
+		else
+			append_char_to_result(input, &pos, &result);
+	}
+	return (result);
 }
