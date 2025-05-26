a78f32db9dd2750f9b2a0c6ba1082336663e6ec1
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-05-26 19:46:20 +0200

    refactor: improve variable handling and error management in expand_vars function

--- src/parser/expand_vars.c
+++ src/parser/expand_vars.c
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 13:42:50 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/05/26 19:29:11 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/05/26 19:45:36 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "minishell.h"
 #include "env.h"
 #include <stdlib.h>
+#include "executor.h"
 
 static int  handle_exit_status(const char *input, size_t *i, char **res, t_shell *sh);
 static int  handle_env_var(const char *input, size_t *i, char **res, t_shell *sh);
@@ -35,102 +36,94 @@ static char *get_env_value(const char *var_name, t_env_list *env_list)
     return ("");
 }
 
-// Главная функция — сканирует строку и вызывает хендлеры:
+// 1) $?
+static int handle_exit_status(const char *input, size_t *pos, char **result, t_shell *sh)
+{
+    if (input[*pos + 1] != '?')
+        return (FAILURE);
+
+    char *status_str = ft_itoa(sh->last_status);
+    if (!status_str)
+        return (FAILURE);
+
+    char *tmp = *result;
+    *result = ft_strjoin(*result, status_str);
+    free(tmp);
+    free(status_str);
+
+    *pos += 2;
+    return (SUCCESS);
+}
+
+// 2) $NAME
+static int handle_env_var(const char *input, size_t *pos, char **result, t_shell *sh)
+{
+    size_t j = *pos + 1;
+    if (!(ft_isalnum((unsigned char)input[j]) || input[j] == '_'))
+        return (FAILURE);
+
+    while (input[j] && (ft_isalnum((unsigned char)input[j]) || input[j] == '_'))
+        j++;
+
+    char *var_name = ft_substr(input, *pos + 1, j - (*pos + 1));
+    if (!var_name)
+        return (FAILURE);
+
+    char *val = get_env_value(var_name, sh->env_list);
+    free(var_name);
+
+    char *tmp = *result;
+    *result = ft_strjoin(*result, val);
+    free(tmp);
+
+    *pos = j;
+    return (SUCCESS);
+}
+
+// 3) just symbol '$'
+static int handle_literal_dollar(const char *input, size_t *pos, char **result)
+{
+    char *tmp = *result;
+    
+    if (input[*pos] != '$')
+        return (FAILURE);
+    *result = ft_strjoin(tmp, "$");
+    free(tmp);
+
+    *pos += 1;
+    return (SUCCESS);
+}
+
 char *expand_vars(const char *input, t_shell *sh)
 {
     char   *result;
-    size_t  i;
+    size_t  pos;
 
     result = ft_strdup("");
     if (!result)
         return (NULL);
 
-    i = 0;
-    while (input[i])
+    pos = 0;
+    while (input[pos])
     {
-        if (input[i] == '$')
+        if (input[pos] == '$')
         {
-            if (handle_exit_status(input, &i, &result, sh))
+            if (handle_exit_status(input, &pos, &result, sh) == SUCCESS)
                 continue;
-            if (handle_env_var(input, &i, &result, sh))
+            if (handle_env_var(input, &pos, &result, sh) == SUCCESS)
                 continue;
-            if (handle_literal_dollar(input, &i, &result))
+            if (handle_literal_dollar(input, &pos, &result) == SUCCESS)
                 continue;
         }
         else
         {
-            // копируем обычный символ
-            char buf[2] = { input[i], '\0' };
+            char buf[2] = { input[pos], '\0' };
             char *tmp   = result;
 
             result = ft_strjoin(result, buf);
             free(tmp);
-            i++;
+            pos++;
         }
     }
     return (result);
-}
-
-// 1) $?
-static int handle_exit_status(const char *input, size_t *i, char **res, t_shell *sh)
-{
-    if (input[*i + 1] != '?')
-        return (0);
-
-    char *status_str = ft_itoa(sh->last_status);
-    if (!status_str)
-        return (0);
-
-    // result = result + status_str
-    char *tmp = *res;
-    *res = ft_strjoin(*res, status_str);
-    free(tmp);
-    free(status_str);
-
-    *i += 2;
-    return (1);
-}
-
-// 2) $NAME
-static int handle_env_var(const char *input, size_t *i, char **res, t_shell *sh)
-{
-    size_t j = *i + 1;
-
-    // проверяем, что за $ идёт допустимый первый символ имени
-    if (!(ft_isalnum((unsigned char)input[j]) || input[j] == '_'))
-        return (0);
-
-    // ищем конец имени
-    while (input[j] && (ft_isalnum((unsigned char)input[j]) || input[j] == '_'))
-        j++;
-
-    // вырезаем имя
-    char *var_name = ft_substr(input, *i + 1, j - (*i + 1));
-    if (!var_name)
-        return (0);
-
-    // ищем в вашем списке env
-    char *val = get_env_value(var_name, sh->env_list);
-    free(var_name);
-
-    // конкатенируем значение
-    char *tmp = *res;
-    *res = ft_strjoin(*res, val);
-    free(tmp);
-
-    *i = j;
-    return (1);
-}
-
-// 3) просто литерал '$'
-static int handle_literal_dollar(const char *input, size_t *i, char **res)
-{
-    (void)input;
-    // здесь мы гарантированно на input[*i] == '$'
-    char *tmp = *res;
-    *res = ft_strjoin(*res, "$");
-    free(tmp);
-
-    *i += 1;
-    return (1);
 }
