b46be603c576cbabc9be55b86fc544ebb126c527
Extraden <53974460+Extraden@users.noreply.github.com>
2025-05-28 23:26:42 +0200

    refactor: update validation logic in builtins and add is_valid function for unset

--- src/builtin/builtin_utils.c
+++ src/builtin/builtin_utils.c
@@ -6,12 +6,13 @@
 /*   By: denissemenov <denissemenov@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 18:52:06 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/05/28 22:14:00 by denissemeno      ###   ########.fr       */
+/*   Updated: 2025/05/28 23:25:28 by denissemeno      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "builtins.h"
+#include "ft_printf.h"
 #include <stddef.h>
 #include <stdlib.h>
 
@@ -26,7 +27,7 @@ void	free_key_value(char *key, char *value)
 t_export_type	is_valid_name(char *arg)
 {
 	if ((ft_isalpha(arg[0]) != 1) && (arg[0] != '_'))
-		return (INVALID);
+		return (ERROR);
 	arg++;
 	while (*arg)
 	{
@@ -35,8 +36,27 @@ t_export_type	is_valid_name(char *arg)
 		if (*arg == '=')
 			return (EXPORT);
 		if ((ft_isalnum(*arg) != 1) && (*arg != '_'))
-			return (INVALID);
+			return (ERROR);
 		arg++;
 	}
 	return (EXPORT);
+}
+t_unset_type	is_valid(char *arg)
+{
+	if ((ft_isalpha(arg[0]) != 1) && (arg[0] != '_'))
+	{
+		ft_dprintf(2, "minishell: unset: `%s': not a valid identifier\n", arg);
+		return (INVALID);
+	}
+	arg++;
+	while (*arg)
+	{
+		if ((ft_isalnum(*arg) != 1) && (*arg != '_'))
+		{
+			ft_dprintf(2, "minishell: unset: `%s': not a valid identifier\n", arg);
+			return (INVALID);
+		}
+		arg++;
+	}
+	return (VALID);
 }
