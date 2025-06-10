93beb2441bb6b9491c27cb36c69f0e43c98418e7
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-06-10 13:54:04 +0200

    refactor: simplify token handling logic in lexer function

--- src/parser/lexer.c
+++ src/parser/lexer.c
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 16:33:07 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/06/02 16:15:35 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/06/10 13:52:43 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,6 +98,32 @@ static int	read_dquoted(t_token **tokens, char *input, int *i, int sep_flag)
 	return (0);
 }
 
+static int	handle_token(t_token **tokens, char *input, int *i, int sep_flag)
+{
+	if (input[*i] == '|')
+		handle_pipe(tokens, i, sep_flag);
+	else if (input[*i] == '<')
+		handle_less(tokens, input, i, sep_flag);
+	else if (input[*i] == '>')
+		handle_greater(tokens, input, i, sep_flag);
+	else if (input[*i] == '\'')
+	{
+		if (read_squoted(tokens, input, i, sep_flag))
+			return (1);
+	}
+	else if (input[*i] == '"')
+	{
+		if (read_dquoted(tokens, input, i, sep_flag))
+			return (1);
+	}
+	else
+	{
+		if (read_unquoted(tokens, input, i, sep_flag))
+			return (1);
+	}
+	return (0);
+}
+
 t_token	*lexer(char *input)
 {
 	t_token	*tokens;
@@ -115,44 +141,12 @@ t_token	*lexer(char *input)
 		{
 			last_was_space = 1;
 			i++;
+			continue ;
 		}
-		else
-		{
-			sep_flag = last_was_space;
-			if (input[i] == '|')
-			{
-				handle_pipe(&tokens, &i, sep_flag);
-				last_was_space = 0;
-			}
-			else if (input[i] == '<')
-			{
-				handle_less(&tokens, input, &i, sep_flag);
-				last_was_space = 0;
-			}
-			else if (input[i] == '>')
-			{
-				handle_greater(&tokens, input, &i, sep_flag);
-				last_was_space = 0;
-			}
-			else if (input[i] == '\'')
-			{
-				if (read_squoted(&tokens, input, &i, sep_flag) == 1)
-					return (free_tokens(tokens), NULL);
-				last_was_space = 0;
-			}
-			else if (input[i] == '"')
-			{
-				if (read_dquoted(&tokens, input, &i, sep_flag) == 1)
-					return (free_tokens(tokens), NULL);
-				last_was_space = 0;
-			}
-			else
-			{
-				if (read_unquoted(&tokens, input, &i, sep_flag))
-					return (free_tokens(tokens), NULL);
-				last_was_space = 0;
-			}
-		}
+		sep_flag = last_was_space;
+		if (handle_token(&tokens, input, &i, sep_flag))
+			return (free_tokens(tokens), NULL);
+		last_was_space = 0;
 	}
 	return (tokens);
 }
