f7f05f7f477c64da0e3975be7864fc7bd16a007e
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-06-11 12:44:26 +0200

    style: improve code formatting for consistency in external command execution

--- src/executor/executor_utils.c
+++ src/executor/executor_utils.c
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:19:46 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/06/11 12:36:27 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/06/11 12:44:09 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -183,45 +183,45 @@ int	handle_redirections(t_cmd *cmd, t_shell *sh)
 	return (0);
 }
 
-static void execute_external_command(t_cmd *cmd, t_shell *sh)
+static void	execute_external_command(t_cmd *cmd, t_shell *sh)
 {
-    char *cmd_name;
-    char *full_cmd;
+	char	*cmd_name;
+	char	*full_cmd;
 
-    cmd_name = cmd->args[0];
-    if (cmd_name[0] == '\0')
-    {
-        write(2, "'': command not found\n", 23);
-        exit(CMD_NOT_FOUND);
-    }
-    full_cmd = find_command(cmd_name, sh->env_tab);
-    if (!full_cmd)
-    {
-        write(2, cmd_name, ft_strlen(cmd_name));
-        write(2, ": command not found\n", 21);
-        exit(CMD_NOT_FOUND);
-    }
-    if (is_directory(full_cmd))
-    {
-        free(full_cmd);
-        exit(CMD_IS_DIRECTORY);
-    }
-    execve(full_cmd, cmd->args, sh->env_tab);
-    free(full_cmd);
-    error_exit("execve");
+	cmd_name = cmd->args[0];
+	if (cmd_name[0] == '\0')
+	{
+		write(2, "'': command not found\n", 23);
+		exit(CMD_NOT_FOUND);
+	}
+	full_cmd = find_command(cmd_name, sh->env_tab);
+	if (!full_cmd)
+	{
+		write(2, cmd_name, ft_strlen(cmd_name));
+		write(2, ": command not found\n", 21);
+		exit(CMD_NOT_FOUND);
+	}
+	if (is_directory(full_cmd))
+	{
+		free(full_cmd);
+		exit(CMD_IS_DIRECTORY);
+	}
+	execve(full_cmd, cmd->args, sh->env_tab);
+	free(full_cmd);
+	error_exit("execve");
 }
 
-void execute_child(t_cmd *cmd, t_shell *sh)
+void	execute_child(t_cmd *cmd, t_shell *sh)
 {
-    if (handle_redirections(cmd, sh) < 0)
-    {
-        sh->last_status = 1;
-        exit(1);
-    }
-    if (!cmd->args || !cmd->args[0])
-        exit(0);
-    if (run_builtin(cmd, sh) == -1)
-        execute_external_command(cmd, sh);
+	if (handle_redirections(cmd, sh) < 0)
+	{
+		sh->last_status = 1;
+		exit(1);
+	}
+	if (!cmd->args || !cmd->args[0])
+		exit(0);
+	if (run_builtin(cmd, sh) == -1)
+		execute_external_command(cmd, sh);
 }
 
 void	setup_child_fds(int prev_fd, t_pipe pd, t_cmd *cmd)
