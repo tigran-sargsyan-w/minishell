f568d8197b80e67ae239de250aa66390e3b55ced
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-06-11 12:25:14 +0200

    refactor: improve redirection handling by separating file opening and descriptor duplication

--- src/executor/executor_utils.c
+++ src/executor/executor_utils.c
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:19:46 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/06/11 12:08:39 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/06/11 12:23:58 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,49 +115,52 @@ static int	handle_heredoc(t_redir *redir, t_shell *sh)
 	return (0);
 }
 
-static int	apply_one_redir(t_redir *redir, t_shell *sh)
+// Открывает файл для редиректа и выводит perror при ошибке
+static int open_redirection_file(t_redir *redir)
 {
-	int	fd;
-	int	ret;
+    int fd;
 
-	fd = -1;
-	if (redir->type == REDIR_HEREDOC)
-		return (handle_heredoc(redir, sh));
-	// 2) open the file with the required mode
-	if (redir->type == REDIR_IN)
-	{
-		fd = open(redir->filename, O_RDONLY);
-	}
-	else if (redir->type == REDIR_OUT)
-	{
-		fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	}
-	else if (redir->type == REDIR_APPEND)
-	{
-		fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	}
-	if (fd < 0)
-	{
-		perror(redir->filename);
-		return (-1);
-	}
-	// 3) duplicate the descriptor into stdin or stdout
-	if (redir->type == REDIR_IN)
-	{
-		ret = dup2(fd, STDIN_FILENO);
-	}
-	else
-	{
-		ret = dup2(fd, STDOUT_FILENO);
-	}
-	if (ret < 0)
-	{
-		perror("dup2");
-		close(fd);
-		return (-1);
-	}
-	close(fd);
-	return (0);
+    if (redir->type == REDIR_IN)
+        fd = open(redir->filename, O_RDONLY);
+    else if (redir->type == REDIR_OUT)
+        fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    else if (redir->type == REDIR_APPEND)
+        fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd < 0)
+        perror(redir->filename);
+    return fd;
+}
+
+// Дублирует fd в stdin или stdout, закрывает и возвращает 0 или -1
+static int redirect_fd_to_stdio(int fd, t_redir *redir)
+{
+    int ret;
+
+    if (redir->type == REDIR_IN)
+        ret = dup2(fd, STDIN_FILENO);
+    else
+        ret = dup2(fd, STDOUT_FILENO);
+    if (ret < 0)
+    {
+        perror("dup2");
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    return 0;
+}
+
+// Применяет один редирект (включая heredoc)
+static int apply_one_redir(t_redir *redir, t_shell *sh)
+{
+    int fd;
+
+    if (redir->type == REDIR_HEREDOC)
+        return handle_heredoc(redir, sh);
+    fd = open_redirection_file(redir);
+    if (fd < 0)
+        return -1;
+    return redirect_fd_to_stdio(fd, redir);
 }
 
 int	handle_redirections(t_cmd *cmd, t_shell *sh)
