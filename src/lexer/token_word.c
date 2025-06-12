bed982b9a4de05f89c6a687b316bebfba7f9ceea
tigran-sargsyan-w <tigran-sargsyan-w@gmail.com>
2025-06-12 15:39:46 +0200

    fix: prevent memory leaks by freeing duplicated strings in token reading functions

--- src/lexer/token_word.c
+++ src/lexer/token_word.c
@@ -6,29 +6,33 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 14:05:37 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/06/11 00:05:28 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/06/12 15:39:09 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "lexer.h"
+#include <stdlib.h>
 
 int	read_unquoted(t_token **tokens, char *input, int *i, int sep_flag)
 {
-	int	start;
+	int		start;
+	char	*dup_word;
 
 	start = *i;
 	while (input[*i] && !is_space(input[*i]) && !is_special(input[*i])
 		&& input[*i] != '\'' && input[*i] != '"')
 		(*i)++;
-	add_token(tokens, TOK_WORD, ft_strndup(input + start, *i - start),
-		sep_flag);
+	dup_word = ft_strndup(input + start, *i - start);
+	add_token(tokens, TOK_WORD, dup_word, sep_flag);
+	free(dup_word);
 	return (0);
 }
 
 int	read_squoted(t_token **tokens, char *input, int *i, int sep_flag)
 {
-	int	start;
+	int		start;
+	char	*dup_word;
 
 	start = ++(*i);
 	while (input[*i] && input[*i] != '\'')
@@ -38,15 +42,17 @@ int	read_squoted(t_token **tokens, char *input, int *i, int sep_flag)
 		ft_dprintf(2, "minishell: syntax error: unclosed single quote\n");
 		return (1);
 	}
-	add_token(tokens, TOK_SQUOTED, ft_strndup(input + start, *i - start),
-		sep_flag);
+	dup_word = ft_strndup(input + start, *i - start);
+	add_token(tokens, TOK_SQUOTED, dup_word, sep_flag);
 	(*i)++;
+	free(dup_word);
 	return (0);
 }
 
 int	read_dquoted(t_token **tokens, char *input, int *i, int sep_flag)
 {
-	int	start;
+	int		start;
+	char	*dup_word;
 
 	start = ++(*i);
 	while (input[*i] && input[*i] != '"')
@@ -56,8 +62,9 @@ int	read_dquoted(t_token **tokens, char *input, int *i, int sep_flag)
 		ft_dprintf(2, "minishell: syntax error: unclosed double quote\n");
 		return (1);
 	}
-	add_token(tokens, TOK_DQUOTED, ft_strndup(input + start, *i - start),
-		sep_flag);
+	dup_word = ft_strndup(input + start, *i - start);
+	add_token(tokens, TOK_DQUOTED, dup_word, sep_flag);
 	(*i)++;
+	free(dup_word);
 	return (0);
 }
