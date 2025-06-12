9b4f43b0b06b1ef49d30809d21bfd4acabd34cdc
tigran-sargsyan-w <tigran-sargsyan-w@gmail.com>
2025-06-12 17:24:37 +0200

    fix: free command list on exit to prevent memory leaks

--- src/builtin/run_builtin.c
+++ src/builtin/run_builtin.c
@@ -6,20 +6,25 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:51:33 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/06/11 10:35:18 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/06/12 17:23:42 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include "libft.h"
+#include "env.h"
 #include "minishell.h"
 #include "parser.h"
 #include <stdlib.h>
+#include <readline/history.h>
 
 static int	handle_exit(t_shell *sh, t_cmd *cmd)
 {
 	if (builtin_exit(sh, cmd->args) == 1)
+	{
+		free_cmd_list(cmd);
 		exit(sh->last_status);
+	}
 	else
 		return (sh->last_status);
 }
