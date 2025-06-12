c4e4e70ac69c9721b46404e70072e8e5f2055f77
dsemenov <dsemenov@student.42lyon.fr>
2025-06-12 03:57:16 +0200

    refactor: implement add_env_entry function for environment variable management

--- src/env/env_list_utils.c
+++ src/env/env_list_utils.c
@@ -6,7 +6,7 @@
 /*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 14:07:38 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/06/12 03:15:01 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/06/12 03:48:19 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,37 @@ t_env_list	*set_value(t_env_list *node, char *value)
 		free(node->value);
 	node->value = dup;
 	return (node);
+}
+
+static int	entry_fail(char *key, char *value)
+{
+	if (key)
+		free(key);
+	if (value)
+		free(value);
+	perror("minishell");
+	return (1);
+}
+
+int	add_env_entry(char *entry, t_env_list **list)
+{
+	char		*equal_sign;
+	char		*key;
+	char		*value;
+	t_env_list	*node;
+
+	equal_sign = ft_strchr(entry, '=');
+	if (!equal_sign)
+		return (0);
+	key = ft_substr(entry, 0, equal_sign - entry);
+	if (!key)
+		return (entry_fail(NULL, NULL));
+	value = ft_strdup(equal_sign + 1);
+	if (!value)
+		return (entry_fail(key, NULL));
+	node = lst_create_node(key, value);
+	if (!node)
+		return (entry_fail(key, value));
+	lst_add_end(list, node);
+	return (0);
 }
