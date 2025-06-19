3f331fb19a8f15fa3b392546100bd16ab1ae2707
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-06-19 17:00:36 +0200

    docs:  in env_list_to_tab.c

--- src/env/env_list_to_tab.c
+++ src/env/env_list_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 16:00:25 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/06/14 18:56:52 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/06/19 16:59:38 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Joins two strings with an '=' in between.
+ * Allocates memory for the new string and returns it.
+ * @param s1 The first string (key).
+ * @param s2 The second string (value).
+ * @return A new string containing "s1=s2", or NULL on failure.
+ */
 static char	*ft_strjoin3(char const *s1, char const *s2)
 {
 	size_t	total_len;
@@ -34,6 +41,12 @@ static char	*ft_strjoin3(char const *s1, char const *s2)
 	return (res);
 }
 
+/**
+ * @brief Returns an empty environment tab.
+ * Allocates memory for a single NULL pointer and returns it.
+ * @return A pointer to an array containing a single NULL pointer,
+ * or NULL on failure.
+ */
 static char	**return_empty_tab(void)
 {
 	char	**tab;
@@ -48,6 +61,13 @@ static char	**return_empty_tab(void)
 	return (tab);
 }
 
+/**
+ * @brief Fills the environment tab with key-value pairs from the list.
+ * @param list Pointer to the environment list.
+ * @param tab Pointer to the environment tab.
+ * @param len The number of elements to fill.
+ * @return A pointer to the filled environment tab, or NULL on failure.
+ */
 static char	**fill_tab(t_env_list **list, char **tab, size_t len)
 {
 	t_env_list	*tmp;
@@ -70,6 +90,16 @@ static char	**fill_tab(t_env_list **list, char **tab, size_t len)
 	return (tab);
 }
 
+/**
+ * @brief Converts an environment list to a tab of strings.
+ * This function iterates through the environment list,
+ * concatenates each key-value pair into a string of the form "key=value",
+ * and stores the result in a newly allocated array.
+ * @param env_list Pointer to the environment list.
+ * @return A pointer to an array of strings representing the environment,
+ * or NULL on failure. If the list is empty, returns an array with a single
+ * NULL pointer.
+ */
 char	**env_list_to_tab(t_env_list **env_list)
 {
 	char	**env_tab;
