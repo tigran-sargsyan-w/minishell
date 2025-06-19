10c13d996f8ef82976af0baa7d52b0d3beebdaba
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-06-19 23:19:03 +0200

    docs: update author information and enhance function documentation in signals.c

--- src/minishell_utils/signals.c
+++ src/minishell_utils/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 21:58:12 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/06/10 21:06:22 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/06/19 23:18:38 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@
 
 volatile sig_atomic_t	g_signo = 0;
 
+/**
+ * @brief Signal handler for SIGINT (Ctrl+C).
+ * It sets a flag to indicate that the signal was received.
+ * @param signo The signal number (not used in this handler).
+ */
 void	sigint_handler(int signo)
 {
 	(void)signo;
@@ -24,11 +29,21 @@ void	sigint_handler(int signo)
 	g_signo = 1;
 }
 
+/**
+ * @brief A dummy event handler that does nothing.
+ * This is used to set the readline event hook to avoid unwanted behavior.
+ * @return Always returns 0.
+ */
 int	event_nothing(void)
 {
 	return (0);
 }
 
+/**
+ * @brief Sets up signal handlers for the shell.
+ * It sets the SIGINT handler to `sigint_handler` and ignores SIGQUIT.
+ * It also sets the readline event hook to `event_nothing`.
+ */
 void	setup_signal_handlers(void)
 {
 	rl_event_hook = event_nothing;
