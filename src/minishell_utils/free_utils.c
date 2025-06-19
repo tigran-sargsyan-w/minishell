cf7457be0e60cb09cf2853062bdc9b0d77debb8a
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-06-19 23:14:50 +0200

    docs: update function documentation in free_utils.c

--- src/minishell_utils/free_utils.c
+++ src/minishell_utils/free_utils.c
@@ -6,7 +6,7 @@
 /*   By: tsargsya <tsargsya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 22:39:27 by tsargsya          #+#    #+#             */
-/*   Updated: 2025/06/18 15:18:29 by tsargsya         ###   ########.fr       */
+/*   Updated: 2025/06/19 23:14:24 by tsargsya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 #include "parser.h"
 #include <stdlib.h>
 
+/**
+ * @brief Frees a linked list of redirection nodes.
+ * @param redir Pointer to the head of the linked list of redirections.
+ */
 static void	free_redirs(t_redir *redir)
 {
 	t_redir	*next;
@@ -27,6 +31,11 @@ static void	free_redirs(t_redir *redir)
 	}
 }
 
+/**
+ * @brief Frees a linked list of command nodes.
+ * Each command node contains arguments and redirections.
+ * @param cmd Pointer to the head of the linked list of commands.
+ */
 void	free_cmd_list(t_cmd *cmd)
 {
 	t_cmd	*next;
@@ -54,6 +63,11 @@ void	free_cmd_list(t_cmd *cmd)
 	}
 }
 
+/**
+ * @brief Frees an array of environment variables.
+ * Each variable is a string, and the array is terminated by a NULL pointer.
+ * @param env_tab Pointer to the array of environment variables.
+ */
 void	free_env_tab(char **env_tab)
 {
 	size_t	i;
@@ -69,12 +83,23 @@ void	free_env_tab(char **env_tab)
 	free(env_tab);
 }
 
+/**
+ * @brief Frees all environment variables in the shell structure.
+ * It clears the linked list of environment variables and frees the array.
+ * @param sh Pointer to the shell structure containing the environment list.
+ */
 void	free_all_env(t_shell *sh)
 {
 	lst_clear(&sh->env_list);
 	free_env_tab(sh->env_tab);
 }
 
+/**
+ * @brief Frees all resources used by the shell.
+ * This includes freeing the command list, environment variables,
+ * and closing all file descriptors.
+ * @param sh Pointer to the shell structure containing all resources.
+ */
 void	free_all_resources(t_shell *sh)
 {
 	free_cmd_list(sh->cmd_list);
